91b4a9eb618d73e7d9ec87ea38fbb1bb7198491f
fraczo <jacek.rawiak@hotmail.com>
2015-09-16 15:12:40 +0200

    ok

--- STAFix24_Biuromagda/EventReceivers/tabKlienciER/tabKlienciER.cs
+++ STAFix24_Biuromagda/EventReceivers/tabKlienciER/tabKlienciER.cs
@@ -17,39 +17,83 @@ namespace EventReceivers.tabKlienciER
        /// </summary>
        public override void ItemUpdated(SPItemEventProperties properties)
        {
+           this.EventFiringEnabled = false;
+
            try
            {
                SPListItem item = properties.ListItem;
                SPWeb web = properties.Web;
 
-               string typKlienta = item["ContentType"].ToString();
-               switch (typKlienta)
-               {
-                   case "KPiR":
-                   case "KSH":
-                       string folderName = item["colNazwaSkrocona"] != null ? item["colNazwaSkrocona"].ToString() : string.Empty;
-                       string status = item["enumStatus"] != null ? item["enumStatus"].ToString() : string.Empty;
+               Update_LookupRefFields(item);
 
-                       if (status == "Aktywny" && !String.IsNullOrEmpty(folderName))
-                       {
-                           int docId = BLL.libDokumenty.Ensure_FolderExist(web, folderName);
-                           int currDocId = item["_DocumentId"]!=null?int.Parse(item["_DocumentId"].ToString()):0;
-
-                           if (docId>0 && currDocId!=docId)
-                           {
-                               item["_DocumentId"] = docId;
-                               item.Update();
-                           }
-                       }
-                       break;
-
-                   default:
-                       break;
-               }
+               Update_FolderInLibrary(item, web);
            }
            catch (Exception ex)
            {
                var result = ElasticEmail.EmailGenerator.ReportError(ex, properties.WebUrl.ToString());
+           }
+           finally
+           {
+               this.EventFiringEnabled = true;
+           }
+       }
+
+       private static void Update_LookupRefFields(SPListItem item)
+       {
+           // aktualizacja odwołań do lookupów
+           item["_TypZawartosci"] = item["ContentType"].ToString();
+           item["_Biuro"] = item["selBiuro"] != null ? new SPFieldLookupValue(item["selBiuro"].ToString()).LookupValue : string.Empty;
+           item["_ZatrudniaPracownikow"] = item["colZatrudniaPracownikow"] != null && (bool)item["colZatrudniaPracownikow"] ? "TAK" : string.Empty;
+
+           string np = string.Empty;
+           switch (item.ContentType.Name)
+           {
+               case "KPiR":
+               case "KSH":
+               case "Firma":
+                   np = string.Format("{0} NIP:{1}", item.Title, item["colNIP"] != null ? item["colNIP"].ToString() : string.Empty);
+                   break;
+               case "Klient":
+                   np = item["colNazwaSkrocona"].ToString();
+                   break;
+               case "Osoba fizyczna":
+                   string npNazwsko = item["colNazwisko"] != null ? item["colNazwisko"].ToString().Trim() : string.Empty;
+                   string npImie = item["colImie"] != null ? item["colImie"].ToString().Trim() : string.Empty;
+                   string npPESEL = item["colPESEL"] != null ? item["colPESEL"].ToString().Trim() : string.Empty;
+                   np = string.Format("{0} {1} PESEL:{2}", npNazwsko, npImie, npPESEL);
+                   break;
+               default:
+                   break;
+           }
+           item["_NazwaPrezentowana"] = np;
+           item.SystemUpdate();
+       }
+
+       private static void Update_FolderInLibrary(SPListItem item, SPWeb web)
+       {
+           string typKlienta = item["ContentType"].ToString();
+           switch (typKlienta)
+           {
+               case "KPiR":
+               case "KSH":
+                   string folderName = item["colNazwaSkrocona"] != null ? item["colNazwaSkrocona"].ToString() : string.Empty;
+                   string status = item["enumStatus"] != null ? item["enumStatus"].ToString() : string.Empty;
+
+                   if (status == "Aktywny" && !String.IsNullOrEmpty(folderName))
+                   {
+                       int docId = BLL.libDokumenty.Ensure_FolderExist(web, folderName);
+                       int currDocId = item["_DocumentId"] != null ? int.Parse(item["_DocumentId"].ToString()) : 0;
+
+                       if (docId > 0 && currDocId != docId)
+                       {
+                           item["_DocumentId"] = docId.ToString();
+                           item.SystemUpdate();
+                       }
+                   }
+                   break;
+
+               default:
+                   break;
            }
        }
 
--- STAFix24_Biuromagda/STAFix24_Biuromagda_BLL/libDokumenty.cs
+++ STAFix24_Biuromagda/STAFix24_Biuromagda_BLL/libDokumenty.cs
@@ -19,22 +19,34 @@ namespace BLL
 
             SPList list = web.Lists.TryGetList(targetList);
 
-            SPListItem item = list.Items.Cast<SPListItem>()
-                .Where(i => i["LinkFilename"].ToString() == folderName)
-                .FirstOrDefault();
+            //SPListItem item = list.Items.Cast<SPListItem>()
+            //    .Where(i => i.ContentType.Name == "Folder")
+            //    .Where(i => i["LinkFilename"].ToString() == folderName)
+            //    .FirstOrDefault();
+
+            SPListItem item = null;
+
+            foreach (SPListItem fitem in list.Folders)
+            {
+                if (fitem.Folder.Exists && fitem.Name==folderName)
+                {
+                    item = fitem;
+                }
+            }
 
             if (item != null)
             {
                 return item.ID;
             }
+            else
+            {
+                web.AllowUnsafeUpdates = true;
+                SPListItem newItem = list.Items.Add(list.RootFolder.ServerRelativeUrl, SPFileSystemObjectType.Folder, folderName);
+                newItem.SystemUpdate();
+                web.AllowUnsafeUpdates = false;
 
-            web.AllowUnsafeUpdates = true;
-            SPListItem newItem = list.Items.Add("", SPFileSystemObjectType.Folder, folderName);
-            newItem.Update();
-            web.AllowUnsafeUpdates = false;
-
-            return newItem.ID;
-
+                return newItem.ID;
+            }
         }
 
         private static string CleanupFileName(string nazwaPliku)
@@ -44,16 +56,16 @@ namespace BLL
             Regex r = new Regex(string.Format("[{0}]", Regex.Escape(regexSearch)));
 
             nazwaPliku = r.Replace(nazwaPliku, "");
-            
+
             Regex illegalPathChars = new Regex(@"^\.|[\x00-\x1F,\x7B-\x9F,"",#,%,&,*,/,:,<,>,?,\\]+|(\.\.)+|\.$", RegexOptions.Compiled);
-            
-            nazwaPliku =illegalPathChars.Replace(nazwaPliku,"");
+
+            nazwaPliku = illegalPathChars.Replace(nazwaPliku, "");
 
             return nazwaPliku;
 
         }
 
         //static Regex illegalPathChars = new Regex(@"^\.|[\x00-\x1F,\x7B-\x9F,"",#,%,&,*,/,:,<,>,?,\\]+|(\.\.)+|\.$", RegexOptions.Compiled);
-        
+
     }
 }
